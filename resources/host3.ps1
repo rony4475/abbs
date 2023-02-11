f6299a4a499046a45c49daa08e1af0505068c2d5
rony4475 <125116875+rony4475@users.noreply.github.com>
2023-02-11 19:33:56 +0600

    Update host3.ps1

--- resources/host3.ps1
+++ resources/host3.ps1
@@ -1,7 +1,7 @@
 cd C:\Resource
 C:\Windows\System32\cmd.exe /c dir
 copy ngrok.exe C:\Windows\ngrok.exe
-ngrok.exe config add-authtoken 1jlQ3vQwtRHGQaZ4lkIhaslLt31_5knGeAyKG3dvVJbt7nuRr
+ngrok.exe config add-authtoken 2Lautk7jBnnqifDwZgMqfpG79vE_74WhZa6d9SZAizbAceGw2
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "UserAuthentication" -Value 1
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
