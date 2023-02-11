e90009784494a406a585a3af95b8f72364e4736d
rony4475 <125116875+rony4475@users.noreply.github.com>
2023-02-11 19:40:21 +0600

    Update start.bat

--- resources/start.bat
+++ resources/start.bat
@@ -1,19 +1,19 @@
 @echo off
-net config server /srvcomment:"Fleter.." > out.txt 2>&1 >nul
+net config server /srvcomment:"rony4475.." > out.txt 2>&1 >nul
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user Fleter Admin@ys?0235 /add >nul
-net localgroup administrators Fleter /add >nul
-net user Fleter /active:yes >nul
+net user Adin Admin@235 /add >nul
+net localgroup administrators rony4475 /add >nul
+net user rony4475 /active:yes >nul
 net user installer /delete >nul
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
-ICACLS C:\Windows\Temp /grant Fleter:F >nul
-ICACLS C:\Windows\installer /grant Fleter:F >nul
+ICACLS C:\Windows\Temp /grant rony4475:F >nul
+ICACLS C:\Windows\installer /grant rony4475:F >nul
 echo Successfully installed!
 echo IP:
 curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url
-echo Username: Fleter
-echo Password: Admin@ys?0235
+echo Username: Adin
+echo Password: Admin@235
 exit
