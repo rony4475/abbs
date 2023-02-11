d178ad58b8e74984b354edd3d40d3cc0e5c7e170
FleterA2 <119168922+FleterA2@users.noreply.github.com>
2023-02-11 05:56:16 +0000

    File

--- resources/loop.bat
+++ resources/loop.bat
@@ -1,7 +1,12 @@
 @echo off
 
 :loop
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels || goto exit
+tasklist /fi "imagename eq ngrok.exe" | find /i "ngrok.exe" >NUL && (
+  curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url
+) || (
+  goto exit
+)
 goto loop
+
 :exit
 exit
