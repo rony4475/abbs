f97c94ba216e9879d787c8f8a8a8f56c527a5378
FleterA2 <119168922+FleterA2@users.noreply.github.com>
2023-02-11 05:42:09 +0000

    File

--- resources/host.ps1
+++ resources/host.ps1
@@ -1,7 +1,6 @@
-Expand-Archive ngrok.zip
-.\ngrok\ngrok.exe config add-authtoken 1jlQ3vQwtRHGQaZ4lkIhaslLt31_5knGeAyKG3dvVJbt7nuRr
+cd C:\Resource
+ngrok.exe config add-authtoken 1jlQ3vQwtRHGQaZ4lkIhaslLt31_5knGeAyKG3dvVJbt7nuRr
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "UserAuthentication" -Value 1
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
-Start-Process Powershell -ArgumentList '-Noexit -Command ".\ngrok\ngrok.exe tcp 3389 --region ap"'
-copy ngrok.exe C:\Windows\ngrok.exe
+Start-Process Powershell -ArgumentList '-Noexit -Command "ngrok.exe tcp 3389 --region ap"'
--- resources/loop.bat
+++ resources/loop.bat
@@ -4,3 +4,4 @@
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels |  || goto exit
 goto loop
 :exit
+exit
