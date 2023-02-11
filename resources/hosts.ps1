1a0f2d9f8071c5e037afac651971ed7021410ea5
FleterA2 <119168922+FleterA2@users.noreply.github.com>
2023-02-11 05:53:18 +0000

    File

--- resources/hosts.ps1
+++ resources/hosts.ps1
@@ -1,4 +1,5 @@
 cd C:\Resource
+C:\Windows\System32\cmd.exe /c dir
 ngrok.exe config add-authtoken 1jlQ3vQwtRHGQaZ4lkIhaslLt31_5knGeAyKG3dvVJbt7nuRr
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "UserAuthentication" -Value 1
